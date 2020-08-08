55cbc2612319629cba6ad5da6c54c9965bac93c7
lixiaoman <807408298@qq.com>
2020-08-08 08:43:33 +0800

    test

--- src/test/java/com/example/demo/rni/Client.java
+++ src/test/java/com/example/demo/rni/Client.java
@@ -8,6 +8,7 @@ import java.rmi.Naming;
  * @date 2020/8/2 13:17
  */
 public class Client {
+
     public static void main(String args[]) throws Exception{
         IOperation iOperation= (IOperation) Naming.lookup("rmi://127.0.0.1:1099/Operation");
         System.out.println(iOperation.add(1,1));
